b79d357a67736f6053b4d5ce1010ad28cd231ef3
Elise Meike <techgirl15@users.noreply.github.com>
2017-09-27 21:02:36 -0700

    Added URL compatibility and expanded analysis
    Now can take in a text via URL or file. Also finds average sentence length.

--- textparsing.py
+++ textparsing.py
@@ -4,8 +4,7 @@
 
 
 import nltk
-#nltk.download()
-#from nltk.book import text6
+from urllib import request
 
 
 #divides the number of unique words by the total word count of the text (code modified from nltk.org) 
@@ -15,24 +14,37 @@ def lexical_diversity(text):
 def percentage(count, total):
     return 100 * count / total
 
-#using already downloaded text (code modified from technaverbascripta.wordpress.com)
-f = open('PridePrejudice.txt', mode = 'r')
-text = f.read()
-nltk_text = text.split()
-final_text = nltk.Text(nltk_text)
+def avg_sentence_length(text):
+	sentences = text.count('.') + text.count('!') + text.count('?')
+	return len(text) / sentences
+
+def get_text(url = None):
+	if url is None:
+		#using already downloaded text (code modified from technaverbascripta.wordpress.com)
+		f = open('PridePrejudice.txt', mode = 'r')
+		raw_text = f.read()
+	else:
+		#getting and tokenizing text from a URL (code modified from nltk.org)
+		response = request.urlopen(url)
+		raw_text = response.read().decode('utf8')
+		#print(type(raw))
+	tokens = nltk.word_tokenize(raw_text)
+	#print(type(tokens))
+	text = nltk.Text(tokens)
+	#print(type(text))
+	return text
+
+final_text = get_text("http://www.gutenberg.org/files/1342/1342-0.txt")
 
 print("Examining " + str(final_text))
 print("The text is {:,} characters long.".format(len(final_text)))
-print("Lexical diversity = {:,}".format(lexical_diversity(final_text)))
-print("{:03.2f} % of the text is 'the'".format(percentage(final_text.count('the'), len(final_text))))
+print("Lexical diversity = {:03.3f}".format(lexical_diversity(final_text)))
+print("{:03.3f} % of the text is 'the'".format(percentage(final_text.count('the'), len(final_text))))
+print("The average length of a sentence is {:03.2f}".format(avg_sentence_length(final_text)))
 
 
 #finding the occurences of specific words in the text
 words = {"marry", "Elizabeth", "Darcy"}
 
 for word in words:
-	print("'" + word  + "'" + " occurs " + str(final_text.count(word)) + " times.")
-
-
-#concord = text6.concordance(word)
-#print(word + " occurs " str(len(str(concord))) "in the following passages of \n" + text6)
+	print("'{}' occurs {:,} times.".format(word, final_text.count(word)))
