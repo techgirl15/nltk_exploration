fe3e332066806b167b873600f0b27bdfe707f325
Elise Meike <techgirl15@users.noreply.github.com>
2017-10-19 12:56:45 -0700

    Added word length functions
    Implemented functions that look at the most common words and the length in characters of the words.

--- textparsing.py
+++ textparsing.py
@@ -1,5 +1,5 @@
 #textparsing.py 
-#Elise Meike September 2017
+#Elise Meike October 2017
 #experiments with nltk
 
 
@@ -37,6 +37,24 @@ def avg_sentence_length(raw, text):
 	sentences = len(st(raw))
 	return len(text) / sentences
 
+def common_words(text):
+	fdist1 = nltk.FreqDist(text)
+	cwords = fdist1.most_common(50)
+	fdist1.plot(50, cumulative=True)
+	fdist1.plot(50, cumulative=False)
+	return cwords
+
+def word_length(raw, text):
+	fdist2 = nltk.FreqDist(len(w) for w in text)
+	#lwords = fdist2.most_common()
+	#print(lwords)
+	num_chars = len(raw)
+	num_words = len(text)
+	modelength = fdist2.max()
+	avelength = num_chars/num_words
+	#fdist2.plot(20, cumulative=False)
+	return modelength, avelength
+
 def occurrences(text, words):
 	#finding the occurences of specific words in the text
 	#lower_tokens = nltk.word_tokenize(raw.lower())
@@ -46,10 +64,13 @@ def occurrences(text, words):
 def analyze(raw, text, words):
 	print("Examining " + str(text))
 	print("The text is {:,} characters long.".format(len(text)))
-	print("Lexical diversity = {:03.3f}".format(lexical_diversity(text)))
+	print("Lexical diversity (unique words/total) = {:03.3f}".format(lexical_diversity(text)))
 	print("{:03.3f} % of the text is 'the'".format(percentage(text.count('the'), len(text))))
-	print("The average length of a sentence is {:03.2f}".format(avg_sentence_length(raw, text)))
-	occurrences(text, words)
+	print("The average length of a sentence is {:03.2f} words.".format(avg_sentence_length(raw, text)))
+	print("The most common word length is {} characters.".format(word_length(raw, text)[0]))
+	print("The average word length is {:03.2f} characters.".format(word_length(raw, text)[1]))
+	#occurrences(text, words)
+
 
 
 #let's do the thing
