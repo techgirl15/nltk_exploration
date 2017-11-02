25ca6a509677689a14d25a90e99541bef32f5150
Elise Meike <techgirl15@users.noreply.github.com>
2017-11-01 21:32:37 -0700

    Revert "added noun count function"
    This reverts commit 985b6cd6319097e9e54f46551d689c3f0683416f.

--- textparsing.py
+++ textparsing.py
@@ -1,5 +1,5 @@
 #textparsing.py 
-#Elise Meike September 2017
+#Elise Meike October 2017
 #experiments with nltk
 
 
@@ -37,9 +37,9 @@ def avg_sentence_length(raw, text):
 	sentences = len(st(raw))
 	return len(text) / sentences
 
-def common_words(text, x):
+def common_words(text):
 	fdist1 = nltk.FreqDist(text)
-	cwords = fdist1.most_common(x)
+	cwords = fdist1.most_common(50)
 	fdist1.plot(50, cumulative=True)
 	fdist1.plot(50, cumulative=False)
 	return cwords
@@ -61,27 +61,15 @@ def occurrences(text, words):
 	for word in words:
 		print("'{}' occurs {:,} times.".format(word, text.count(word)))
 
-def nouns(raw):
-	sentences = nltk.sent_tokenize(raw)
-	nouns = []
-	for sentence in sentences:
-		for word,pos in nltk.pos_tag(nltk.word_tokenize(str(sentence))):
-			if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
-				nouns.append(word)
-	return nouns
-
-def analyze(raw, text, words, x):
+def analyze(raw, text, words):
 	print("Examining " + str(text))
 	print("The text is {:,} characters long.".format(len(text)))
 	print("Lexical diversity (unique words/total) = {:03.3f}".format(lexical_diversity(text)))
 	print("{:03.3f} % of the text is 'the'".format(percentage(text.count('the'), len(text))))
-	print("The mean length of a sentence is {:03.2f} words.".format(avg_sentence_length(raw, text)))
+	print("The average length of a sentence is {:03.2f} words.".format(avg_sentence_length(raw, text)))
 	print("The most common word length is {} characters.".format(word_length(raw, text)[0]))
-	print("The mean word length is {:03.2f} characters.".format(word_length(raw, text)[1]))
-	print("The {} most common words and there number of times they occur are {}".format(x, common_words(text, x)))
+	print("The average word length is {:03.2f} characters.".format(word_length(raw, text)[1]))
 	#occurrences(text, words)
-	print("There are {:,} nouns in the text".format(len(nouns(raw))))
-
 
 
 
@@ -89,6 +77,5 @@ def analyze(raw, text, words, x):
 raw_words = get_text("http://www.gutenberg.org/files/1342/1342-0.txt")
 final_text = tokenize_text(raw_words.lower())
 words = {"the","a","be","to","of"}
-x = 10
 
-analyze(raw_words, final_text, words, x)
+analyze(raw_words, final_text, words)
